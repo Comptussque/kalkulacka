8d8537f6aa7d043ce5a0310a708ac73401cd6348
Josef Mikulka <mik39147@lp603.spseol.cz>
2023-01-23 14:10:11 +0100

    add buttons, add math operations, add button functions

--- main.py
+++ main.py
@@ -35,26 +35,104 @@ class Application(tk.Tk):
         self.bind("<Escape>", self.quit)
         self.entry = MyEntry(self)
         self.entry.pack()
-        self.listbox = tk.Listbox(self)
+        self.listbox = tk.Listbox(self, selectmode="multiple")
         self.listbox.pack()
+        # Error messageP
+        self.errorStr = tk.StringVar()
+        self.error_info = tk.Label(self, textvariable=self.errorStr)
+        self.error_info.pack()
+
+        # Buttons
+        button_width = 15
+        self.button_delete = tk.Button(self, width=button_width, text="DELETE", command=self.delete)
+        self.button_clear = tk.Button(self, width=button_width, text="CLEAR", command=self.clear)
+        self.button_copy = tk.Button(self, width=button_width, text="COPY", command=self.copy)
+        self.button_move_up = tk.Button(
+            self, text="MOVE UP", width=button_width, command=self.move_up)
+        self.button_move_down = tk.Button(
+            self, text="MOVE DOWN", width=button_width, command=self.move_down)
+
+        self.button_delete.pack()
+        self.button_clear.pack()
+        self.button_copy.pack()
+        self.button_move_up.pack()
+        self.button_move_down.pack()
 
         self.entry.bind("<Return>", self.process)
         self.entry.bind("<KP_Enter>", self.process)
 
+        # Dict   ||key: operator     value: amount of operands||
+        self.operators = {
+            # 2 operator funcs
+            "+":    2,
+            "-":    2,
+            "*":    2,
+            "/":    2,
+            "**":   2,
+            "//":   2,
+            "%":    2,
+            # 1 operator funcs
+            "sin":  1,
+            "cos":  1,
+            "tan":  1,
+            "log":  1,
+            "log2": 1,
+            # consts (key = name     value = value)
+            "pi": 3.1415
+        }
+
     def process(self, e: tk.Event):
-        values = self.entry.value.split()
-        for value in values:
-            if value in ["+","-","*","/","//","**","%"]:
-                num1 = self.listbox.get(0)
-                num2 = self.listbox.get(1)
-                res = eval(f"{num1} {value} {num2}")
-                self.listbox.delete(0, 1)
+        _input = self.entry.value.lower().split()
+        self.errorStr.set("")
+        # check for operators
+        for value in _input:
+            if value in self.operators.keys():
+                # check if enough operands
+                if self.operators[value] == 2:
+                    try:
+                        num1 = self.listbox.get(0)
+                        num2 = self.listbox.get(1)
+                        res = eval(f"{num1} {value} {num2}")
+                        self.listbox.delete(0, 1)
+                    except:
+                        self.errorStr.set("Not enough operands!")
+                        break
+                elif self.operators[value] == 1:
+                    num1 = self.listbox.get(0)
+                    res = eval(f"math.{value}({num1})")
+                    self.listbox.delete(0)
+                # if not operator but const
+                else:
+                    res = self.operators[value]
                 self.listbox.insert(0, res)
-            elif value.isalnum():
-                self.listbox.insert(self.listbox.index("end"), value)
             else:
-                self.entry.value = "INVALID INPUT"
+                # check if value is number
+                try:
+                    value = float(value)
+                    self.listbox.insert(self.listbox.index("end"), value)
+                except:
+                    self.errorStr.set("Invalid input!")
             self.entry.value = ""
+
+    # Button functions
+    def delete(self):
+            for select_index in self.listbox.curselection():
+                self.listbox.delete(select_index)
+    def copy(self):
+        pass
+    def clear(self):
+        self.listbox.delete(0, self.listbox.size())
+    def move_up(self):
+        for select_index in self.listbox.curselection():
+            select = self.listbox.get(select_index)
+            self.listbox.delete(select_index)
+            self.listbox.insert(select_index-1, select)
+
+    def move_down(self):
+        for select_index in self.listbox.curselection():
+            select = self.listbox.get(select_index)
+            self.listbox.delete(select_index)
+            self.listbox.insert(select_index+1, select)
 
     def quit(self, event=None):
         super().quit()
